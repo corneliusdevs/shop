ac53be90ffd96e4b7b4f850b9990e7fdfada1975
cdevs <owolabitosin020@gmail.com>
2022-12-21 17:16:36 -0800

    updated imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';;
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-
 import App from './App';
-import {Provider} from "react-redux"
-import { store, persistor} from "./redux/store"
-import { PersistGate} from "redux-persist/integration/react"
+import {Provider} from "react-redux";
+import { store, persistor} from "./redux/store";
+import { PersistGate} from "redux-persist/integration/react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
